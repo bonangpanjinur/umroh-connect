dda3591a7266d04dd6220f8feb43fe3f56dcfa38
Bonang <bpanjinur63@gmail.com>
2026-01-31 10:18:57 +0700

    integrasi

--- src/supabaseConfig.ts
+++ src/supabaseConfig.ts
@@ -1,22 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Kita paksa pakai environment variable di sini
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  console.error("Supabase Env Vars missing!");
+  console.error("Missing Supabase configuration in environment variables.");
 }
 
-export const myCustomSupabase = createClient(
-  supabaseUrl || '', 
-  supabaseKey || '',
-  {
-      auth: {
-        storage: window.localStorage,
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: true,
-      },
-  }
+export const supabase = createClient(
+  supabaseUrl || "",
+  supabaseKey || ""
 );
